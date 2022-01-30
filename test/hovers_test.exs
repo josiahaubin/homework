43b70be0a81ff9d0385fa1f3bb41b54db9522423
josiahaubin <josiahaubin@gmail.com>
2022-01-30 15:59:40 -0700

    added hover tests

---
+++ test/hovers_test.exs
@@ -0,0 +1,29 @@
+defmodule Hovers do
+  # Import Helpers
+  use Hound.Helpers
+  use ExUnit.Case
+
+  # Start Session
+  hound_session()
+
+  test "User hovers first image and can view name of the first image" do
+    # Arrange
+    navigate_to("https://the-internet.herokuapp.com/hovers")
+    # Act
+    move_to({:xpath, "//*[@id='content']/div/div[1]/img"}, 10, 10)
+    # Assert
+    assert element_displayed?({:xpath, "//*[@id='content']/div/div[1]/div/h5"}) == true
+    assert visible_text({:xpath, "//*[@id='content']/div/div[1]/div/h5"}) == "name: user1"
+  end
+
+  test "User hovers second image and is able to select link to view profile of second image" do
+    # Arrange
+    navigate_to("https://the-internet.herokuapp.com/hovers")
+    # Act
+    move_to({:xpath, "//*[@id='content']/div/div[2]/img"}, 10, 10)
+    click({:xpath, "//*[@id='content']/div/div[2]/div/a"})
+    # Assert
+    profileUrl = current_url()
+    assert profileUrl == "https://the-internet.herokuapp.com/users/2"
+  end
+end
