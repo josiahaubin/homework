3ca6b42e667cdcd087c8ec9c56eff55ff8613a0e
josiahaubin <josiahaubin@gmail.com>
2022-01-30 17:22:01 -0700

    Added API tests, deps to make it work

---
+++ test/api_test.exs
@@ -0,0 +1,25 @@
+defmodule API do
+  use ExUnit.Case
+
+  test "GET Get user by real user id" do
+    resp = HTTPoison.get!("https://reqres.in/api/users/2")
+    assert resp.status_code == 200
+  end
+
+  test "GET Get user by bad user id" do
+    resp = HTTPoison.get!("https://reqres.in/api/users/-1")
+    assert resp.status_code == 404
+  end
+
+  test "POST Create user" do
+    body = %{name: "Arnold Schwarzenegger", job: "Professional Bodybuilder"} |> Poison.encode!()
+    resp = HTTPoison.post!("https://reqres.in/api/users", body)
+    assert resp.status_code == 201
+  end
+
+  test "POST Login Failed" do
+    body = %{email: "arnie@lifts.com"} |> Poison.encode!()
+    resp = HTTPoison.post!("https://reqres.in/api/login", body)
+    assert resp.status_code == 400
+  end
+end
